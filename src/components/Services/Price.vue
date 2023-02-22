b7cbcc5c5508b01f20be7fdf2f9058e67f6ffa47
X0lotl <danil200319771@gmail.com>
2023-02-22 19:28:28 +0200

    Add some style to price

--- src/components/Services/Price.vue
+++ src/components/Services/Price.vue
@@ -5,18 +5,12 @@ export default {
     title: String,
     price: String,
     currency: String
-  },
-  setup(props) {
-    let title = props.title;
-    let price = props.price;
-    let currency = props.currency
-    return { title, price };
-  },
+  }
 };
 </script>
 <template>
-  <div class="flex md:my-6 pb-2 justify-between lg:px-20 px-2 border-b-2 border-b-emerald-500 border-dashed md:text-xl sm:text-base text-sm">
-    <h4 class="max-w-2xl">{{ title }}</h4>
-    <p class="mt-auto">{{ price }} {{ currency }}</p>
+  <div class="flex gap-4 md:my-6 pb-2 justify-between lg:px-20 px-2 border-b-2 border-b-emerald-500 border-dashed md:text-xl sm:text-base text-sm">
+    <h4 class="max-w-2xl p-2">{{ title }}</h4>
+    <p class="mt-auto whitespace-nowrap p-2 text-right">{{ price }} {{ currency }}</p>
   </div>
 </template>
