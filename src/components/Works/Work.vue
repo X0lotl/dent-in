2b528d8abe619269b95dada5d1052aaa652a5d83
X0lotl <danil200319771@gmail.com>
2023-03-01 19:22:21 +0200

    fix: add v-if to work

--- src/components/Works/Work.vue
+++ src/components/Works/Work.vue
@@ -12,10 +12,9 @@ export default {
 };
 </script>
 <template>
-  <div
+  <div v-if="this.workData.Img.data.attributes.url"
     :style="{ backgroundImage: `url(${this.workData.Img.data.attributes.url})` }"
-    class="bg-no-repeat bg-cover h-80 m-5 grid place-items-end rounded-xl overflow-hidden"
-  >
+    class="bg-no-repeat bg-cover h-80 m-5 grid place-items-end rounded-xl overflow-hidden">
     <div class="h-3/4 mt-auto w-full bg-gradient-to-t from-black to-transparent">
       <h3 class="text-white mt-44 text-2xl text-center font-bold">
         {{ this.workData.Title }}
